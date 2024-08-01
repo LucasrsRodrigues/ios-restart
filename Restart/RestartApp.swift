86d041e979f015630055795c0d13cb345420fb82
Lucas Rodrigues Santos <lucasrs.rodrigues3@outlook.com>
2024-08-01 15:36:43 -0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Lucas Rodrigues on 01/08/24.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
